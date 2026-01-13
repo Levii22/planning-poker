dddaabdeb1f7844e162adbf4c1b6e8727eada034
Levi <78547824+Levii22@users.noreply.github.com>
2026-01-13 21:55:25 +0200

    Add flying card animation

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:5173',
     'http://127.0.0.1:3000',
-    'https://levii22.github.io/planning-poker'
+    'https://levii22.github.io'
 ];
 
 // Rate limiting
